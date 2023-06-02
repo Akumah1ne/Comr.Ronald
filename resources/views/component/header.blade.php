92df90f6ff3f5a4a36c6d9bec0fe687d647311b8
VanenHaaga <vanihaaga@gmail.com>
2023-06-02 04:11:39 +0400

    prof
    ....

---
+++ resources/views/component/header.blade.php
@@ -0,0 +1,37 @@
+<div class="navbar bg-dark">
+    <div class="container">
+        <ul class="d-flex">
+            <li class="text-white">contact</li>
+            <li class="text-white px-4">careers</li>
+            <li class="text-white">Apply/Enroll</li>
+            <li class="text-white px-4">Powerschool login</li>
+            <li class="text-white">SLC/Assessment</li>
+            <li class="text-white px-4">Report Absence</li>
+            <li class="text-white">Safe UT App</li>
+            <li class="text-white px-4">ARP Esser Fund</li>
+            <span><i class="fa fa-facebook" aria-hidden="true"></i></span>
+        </ul>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row ">
+        <div class="col-12">
+            <img class=" logo w-30 h-30" src="{{ asset('web/images/GW.png') }}" alt="">
+        </div>
+    </div>
+    <ul class="d-flex px-5 mt-3">
+        <li class="text-dark text-uppercase">About</li>
+        <li class="text-dark text-uppercase px-4">Students</li>
+        <li class="text-dark text-uppercase">Parents</li>
+        <li class="text-dark text-uppercase px-4">Organisation</li>
+        <li class="text-dark text-uppercase">Payments</li>
+        <li class="text-dark text-uppercase px-4">lunch info </li>
+        <li class="text-dark text-uppercase">resources</li>
+        <li class="text-dark text-uppercase px-4">events</li>
+        <li class="text-dark text-uppercase px-4">news</li>
+        <li class="text-dark text-uppercase px-4">current frp</li>
+    </ul>
+    <div class="botton float-right mb-2">
+        <button type="button" class="btn btn-danger text-uppercase">apply for enrollment</button>
+    </div>
+</div>
